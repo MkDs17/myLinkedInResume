75c3dca04c0e970b77e24d0a4d425fe794a7595d
MkDs17 <mickael.rassicot@gmail.com>
2020-09-01 12:03:28 +0200

    add import for logo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import './nav.scss';
 
-const Nav = () => {
-  return (
-    <div id="nav">
-      <nav className="flex items-center justify-between flex-wrap px-4 py-2 bg-white shadow">
-        <div className="flex items-center text-white mr-6">
-          <img className="fill-current h-8 mr-2" alt="logo mr-webdev" src="/src/public/assets/img/logo.png" />
-        </div>
-        <div className="flex items-center">
-          <a href="https://cv.mr-webdev.com" target="_blank" rel="noreferrer" className="link-button px-4 py-2 mr-4 shadow-lg rounded-md font-semibold">Mon CV</a>
-        </div>
-      </nav>
-    </div>
-  );
-};
+import logo from '../../public/assets/img/logo.png';
+
+const Nav = () => (
+  <div id="nav">
+    <nav className="flex items-center justify-between flex-wrap px-4 py-2 bg-white shadow">
+      <div className="flex items-center text-white mr-6">
+        <img className="fill-current h-8 mr-2" alt="logo mr-webdev" src={logo} />
+      </div>
+      <div className="flex items-center">
+        <a href="https://cv.mr-webdev.com" target="_blank" rel="noreferrer" className="link-button px-4 py-2 mr-4 shadow-lg rounded-md font-semibold">Mon CV</a>
+      </div>
+    </nav>
+  </div>
+);
 
 export default Nav;
