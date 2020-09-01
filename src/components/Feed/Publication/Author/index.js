2611631d9c6a2afa30a9987434297767ead5a918
MkDs17 <mickael.rassicot@gmail.com>
2020-09-01 12:15:27 +0200

    test import img

--- src/components/Feed/Publication/Author/index.js
+++ src/components/Feed/Publication/Author/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './author.scss';
-import test from '../../../../public/assets/img/publi/avatar/MR.png';
 
 import { calculateTimeBtwTwoDates } from '../../../../utils/funtions';
 
@@ -22,7 +21,7 @@ const Author = ({ author }) => {
       <div>
         <div className="flex">
           <div className="publication-author-avatar">
-            <img className="h-12 w-12 mr-2 object-cover rounded-full" alt="author-avatar" src={test} />
+            <img className="h-12 w-12 mr-2 object-cover rounded-full" alt="author-avatar" src={avatarImg.default} />
           </div>
           <div className="publication-author-infos flex flex-col">
             <div className="flex">
